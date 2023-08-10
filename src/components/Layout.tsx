984e6f732eba81916e249545e0889c24016af1a5
Krister Iversen <krister.iversen@bouvet.no>
2023-08-10 08:40:35 +0200

    installert og testet tailwind

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,15 @@
 import { ReactNode } from "react";
 import { Link } from "react-router-dom";
-import logo from "/logo.svg";
+// import logo from "/logo.svg";
 
 export default function Layout(props: { children: ReactNode }) {
   return (
     <>
-      <nav>
-        <Link to="/">
+      <nav className="flex space-x-2 app-bar">
+        {/* <Link to="/">
           <img src={logo} className="logo" alt="Logo" />
-        </Link>
+        </Link> */}
+        <Link to="/">Hjem</Link>
         <Link to="/locations">Lokasjoner</Link>
         <Link to="/spaces">Hensettingsplasser</Link>
         <Link to="/reservations">Reservasjoner</Link>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,7 @@ import togVedStasjon from "/tog_ved_stasjon.jpg";
 export default function Home() {
   return (
     <>
-
-      <nav className="p-4 w-full mb-4 bg-blue hover:bg-green transition-all">
-
-      </nav>
-
-      <h1>Hensetting</h1>
+      <h1 className="underline">Hensetting</h1>
       <h1>Bilde av Sandnes stasjon</h1>
       <img
         src={togVedStasjon}
